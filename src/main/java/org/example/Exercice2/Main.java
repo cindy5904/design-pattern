15b7092bf5ec8712eed560e4b00f5a3dbcb259c7
Cindy Telmon <telcindy59@gmail.com>
2024-04-29 14:27:18 +0200

    exercice1

--- src/main/java/org/example/Exercice2/Main.java
+++ src/main/java/org/example/Exercice2/Main.java
@@ -3,7 +3,8 @@ package org.example.Exercice2;
 public class Main {
     public static void main(String[] args) {
         Restaurant  restaurantItalien = new Restaurant(new ItalianCuisineFactory());
-        System.out.println(restaurantItalien);
+        restaurantItalien.prepareDinner();
+
 
     }
 }
