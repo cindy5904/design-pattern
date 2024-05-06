65f05d0c462516abc2f559496dc23de928be2a36
Cindy Telmon <telcindy59@gmail.com>
2024-05-06 14:31:58 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/SystemePaiement/NewPaymentProcassor.java
+++ src/main/java/org/example/SystemePaiement/NewPaymentProcassor.java
@@ -2,7 +2,7 @@ package org.example.SystemePaiement;
 
 public class NewPaymentProcassor {
     public void authenticate(String apiKey){
-        System.out.println("Utilisateur"+apiKey);
+        System.out.println("Utilisateur "+apiKey);
     };
     public void sendPayment(double amount) {
         System.out.println("votre solde est de "+amount);
